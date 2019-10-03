c4936e597693b4d65daeb5677f136c176b9c48cc
EC2 Default User <ec2-user@ip-172-31-17-200.us-east-2.compute.internal>
2019-10-03 20:18:25 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
-
+  helper:all
+  
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
@@ -11,9 +12,44 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    #Find the all possible ratings of the movies
+    @all_ratings = Movie.listRatings
+    
+    #If ratings are passed as parameters then form the ratings key with those
+    if params[:ratings]
+      @def_ratings = params[:ratings]
+    #If ratings not in params pick the ratings filter from session params
+    elsif session[:ratings]
+      @def_ratings = session[:ratings]
+    #If ratings in session params are nill then choose all ratings
+    else
+      @def_ratings = Hash[@all_ratings.collect{|item| [item, '1']}]
+    end
+    
+    #If sort id is passed in params then set sort_key to that value else choose from session params
+    if params[:sort_id]
+      sort_key = params[:sort_id]
+    else
+      sort_key = session[:sort_id]
+    end
+    
+    @title_hilite = ''
+    @date_hilite = ''
+    if sort_key == 'title'
+      #@movies = Movie.ascorder('title')
+      @title_hilite = 'hilite'
+    elsif sort_key == 'release_date'
+      #@movies = Movie.ascorder('release_date')
+      @date_hilite = 'hilite'
+    end
+    
+    session[:ratings] = @def_ratings
+    session[:sort_id] = sort_key
+    
+    @movies = Movie.get_Rated_Movies(@def_ratings.keys(), sort_key)
+    
   end
-
+  
   def new
     # default: render 'new' template
   end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,17 @@
 class Movie < ActiveRecord::Base
+    def self.ascorder(key)
+        #puts @movies
+        @movies = Movie.order(key)
+        return @movies
+    end
+    
+    def self.listRatings
+        list = Movie.uniq.pluck(:rating)
+        return list
+    end
+    
+    # Get the movies in sorted oreder with ratings filter
+    def self.get_Rated_Movies(list, sort_key = nil)
+        @movies = Movie.where(rating: list).order(sort_key)
+    end 
 end
