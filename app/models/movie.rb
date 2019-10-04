d60e872a833178d62c299c042ae8ac89c544a78c
EC2 Default User <ec2-user@ip-172-31-17-200.us-east-2.compute.internal>
2019-10-04 02:04:15 +0000

    part 3 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,18 @@
+  
 class Movie < ActiveRecord::Base
-    def self.ratings
-        # available movie ratings
-        ['R', 'PG-13','G', 'PG'] 
-    end    
+    def self.ascorder(key)
+        #puts @movies
+        @movies = Movie.order(key)
+        return @movies
+    end
+    
+    def self.listRatings
+        list = Movie.uniq.pluck(:rating)
+        return list
+    end
+    
+    # Get the movies in sorted oreder with ratings filter
+    def self.get_Rated_Movies(list, sort_key = nil)
+        @movies = Movie.where(rating: list).order(sort_key)
+    end 
 end
